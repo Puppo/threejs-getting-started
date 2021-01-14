729b0f55060547f9545d9a8f5c2efb8477f530b3
Luca Del Puppo <luca@delpuppo.net>
2021-01-14 11:15:11 +0100

    text

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,62 @@
-import * as THREE from 'three'
+import * as THREE from "three";
+import { Mesh } from "three";
 
-import '../scss/index.scss'
+import "../scss/index.scss";
+import { fontJSON } from "./font";
 
-let scene: THREE.Scene, camera: THREE.Camera, renderer: THREE.Renderer;
+let scene: THREE.Scene,
+  camera: THREE.Camera,
+  renderer: THREE.Renderer,
+  text: THREE.Mesh;
+const ADD = 0.1;
+
+const createText = () => {
+  const titles =
+    "99 bottles of beer on the wall,\n99 bottles of beer.\nTake one down, pass it around,\n98 bottles of beer on the wall,\n98 bottles of beer.\nTake one down, pass it around,\n97 bottles of beer on the wall,\n97 bottles of beer.\nTake one down, pass it around,\n96 bottles of beer on the wall,\n96 bottles of beer.\nTake one down, pass it around,\n95 bottles of beer on the wall,\n95 bottles of beer.";
+  const fontLoader = new THREE.FontLoader();
+  const font = fontLoader.parse(fontJSON);
+
+  const geometry = new THREE.TextGeometry(titles, {
+    font,
+    size: 3,
+    height: 0.1,
+  });
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+  const text = new Mesh(geometry, material);
+  text.position.x = -25;
+  text.rotation.x = -0.9;
+  return text;
+};
 
 const init = () => {
-    scene = new THREE.Scene()
-    scene.background = new THREE.Color('#ededed')
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color("#000000");
 
-    camera = new THREE.PerspectiveCamera(
-        30,
-        window.innerWidth / window.innerHeight,
-        1,
-        1000
-    )
-    camera.position.z = 5
+  text = createText();
+  scene.add(text);
 
-    renderer = new THREE.WebGLRenderer()
-    renderer.setSize(window.innerWidth, window.innerHeight)
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    1,
+    150
+  );
 
-    document.body.append(renderer.domElement)
-}
+  camera.position.set(0, 5, 40);
 
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
+  document.body.append(renderer.domElement);
+};
 
 const mainLoop = () => {
-    renderer.render(scene, camera)
-    requestAnimationFrame(mainLoop);
-}
+  text.position.z -= ADD;
+  text.position.y += ADD / 2;
+  renderer.render(scene, camera);
+  requestAnimationFrame(mainLoop);
+};
 
-init()
-mainLoop()
+init();
+mainLoop();
