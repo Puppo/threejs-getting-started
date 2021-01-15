4a2783d2bac48e13536dd54257e0d16853f0cba5
Luca Del Puppo <luca@delpuppo.net>
2021-01-15 11:55:36 +0100

    normal-material

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import * as THREE from "three";
+import { FaceNormalsHelper } from "three/examples/jsm/helpers/FaceNormalsHelper";
 
 import "../scss/index.scss";
 
-let scene: THREE.Scene, camera: THREE.Camera, renderer: THREE.Renderer;
+let scene: THREE.Scene,
+  camera: THREE.Camera,
+  renderer: THREE.Renderer,
+  mesh: THREE.Mesh,
+  normals: FaceNormalsHelper;
+let ADD = 0.02;
+
+let createGeometry = function () {
+  let geometry = new THREE.BoxGeometry(5, 5, 5);
+  // let geometry = new THREE.SphereGeometry(5, 30, 30);
+  // let geometry = new THREE.TorusGeometry(5, 2, 10, 12);
+
+  let material = new THREE.MeshNormalMaterial({
+    transparent: true,
+    opacity: 0.8,
+  });
+
+  mesh = new THREE.Mesh(geometry, material);
+
+  normals = new FaceNormalsHelper(mesh, 5);
+
+  scene.add(mesh);
+  scene.add(normals);
+};
 
 const init = () => {
   scene = new THREE.Scene();
@@ -18,6 +42,8 @@ const init = () => {
 
   camera.position.set(0, 5, 40);
 
+  createGeometry();
+
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -25,6 +51,10 @@ const init = () => {
 };
 
 const mainLoop = () => {
+  mesh.rotation.x += ADD;
+  mesh.rotation.y += ADD;
+  normals.update();
+
   renderer.render(scene, camera);
   requestAnimationFrame(mainLoop);
 };
